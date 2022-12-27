13811a9b6a42938d6ff0f742732cd59ca3803b60
Titotrapatonni <titovtelexfree@gmail.com>
2022-12-27 21:23:13 +0200

    Complete hw09 before reveiw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,54 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const formEl = document.querySelector('.form');
 const delayEl = document.querySelector('input[name=delay]');
 const stepEl = document.querySelector('input[name=step]');
 const amountEl = document.querySelector('input[name=amount]');
-let position = 0;
-let delay = Number(delayEl.value);
-let step = 0;
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve('Supergood');
-      } else {
-        reject('wff');
-      }
-
-      // console.log(position);
-
-      // console.log(promise);
-    }, delay);
-  });
-
-  promise
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    })
-    .finally(() => {
-      console.log('finally');
-      // delay = Number(delayEl.value) + step;
-      console.log(promise);
-    });
-}
-
 formEl.addEventListener('submit', onSubmit);
 
 function onSubmit(evt) {
   evt.preventDefault();
-  position = 0;
-  step = 0;
-  delay = Number(delayEl.value);
-  console.log('Submit');
+  if (
+    Number(delayEl.value) < 0 ||
+    Number(stepEl.value) < 0 ||
+    Number(amountEl.value) < 0
+  ) {
+    Notify.warning('Value cannot be negative');
+    return;
+  }
+  let position = 0;
+  let step = 0;
+  let delay = Number(delayEl.value);
   for (let i = 1; i <= amountEl.value; i += 1) {
     position += 1;
     step += Number(stepEl.value);
     createPromise(position, delay);
     delay = Number(delayEl.value) + step;
   }
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  promise
+    .then(value => {
+      Notify.success(value);
+    })
+    .catch(err => {
+      Notify.failure(err);
+    });
 }
