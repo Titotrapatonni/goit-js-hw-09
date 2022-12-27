055746d034265824ef600e2737910e7217241814
Titotrapatonni <109283021+Titotrapatonni@users.noreply.github.com>
2022-12-27 12:26:45 +0000

    Deploying to gh-pages from @ Titotrapatonni/goit-js-hw-09@3af0bc3ad8374f9c21fcd3da70fc08b3e0696fa5 🚀

---
+++ 03-promises.99654fab.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault(),console.log("Submit"),position,delay,Math.random()}));
+//# sourceMappingURL=03-promises.99654fab.js.map
